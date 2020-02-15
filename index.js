a6876ea4a1d406c77f30ecf980b697ebe5cba57f
Zhanybek <sadvazha@fit.cvut.cz>
2020-02-15 23:15:29 +0100

    Base implementation

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 'use strict';
 
-const serverless = require('serverless-http');
+const AWS = require('aws-sdk');
 const express = require('express');
+const serverless = require('serverless-http');
+const uuid = require('uuid');
+
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use(express.json());
+
+const POSTS_TABLE = process.env.POSTS_TABLE;
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+app.get('/v1/posts', async function (req, res, next) {
+    const params = {
+        TableName: POSTS_TABLE,
+        Limit: 5,
+    };
+    
+    try {
+        const postList = await dynamoDb.scan(params).promise();
+        res.json({ postList });
+    } catch (e) {
+        res.status(404).json({error});
+    }
+});
+
+app.post('/v1/posts', async function (req, res, next) {
+    if (!req.body.text || typeof req.body.text !== 'string') {
+        res.status(400).json({ error: '"text" must be provided and must be type of string and must not be empty' });
+    }
+
+    const { text } = req.body;
+    const postId = uuid.v4();
+
+    const post = {
+        TableName: POSTS_TABLE,
+        Item: {
+            postId,
+            text,
+        },
+    };
+
+    try {
+        await dynamoDb.put(post).promise();
+        res.json({ postId, text });
+    } catch (err) {
+        res.status(500).json({error});
+    }
 });
 
 module.exports.handler = serverless(app);
