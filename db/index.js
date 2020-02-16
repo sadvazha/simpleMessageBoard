fa4a1e1318a778ecea9ac6867df678294fa0dd82
Zhanybek <sadvazha@fit.cvut.cz>
2020-02-16 14:10:46 +0100

    First test implementation

---
+++ db/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const IS_OFFLINE = process.env.IS_OFFLINE;
+let dynamoDbInstance;
+if (IS_OFFLINE === 'true') {
+  dynamoDbInstance = new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+  })
+  console.log(dynamoDbInstance);
+} else {
+  dynamoDbInstance = new AWS.DynamoDB.DocumentClient();
+};
+
+module.exports = {
+    dynamoDbInstance,
+};
